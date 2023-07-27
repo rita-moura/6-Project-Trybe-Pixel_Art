a4a4e3b70c43a4e866249d5353c59b366775e04f
Rita Moura <ritamoura520@gmail.com>
2023-07-27 17:14:19 -0300

    Feat: altera tamanho do quadro de pixel.

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const divColor0 = document.querySelector('#color-0');
 const divColor1 = document.querySelector('#color-1');
 const divColor2 = document.querySelector('#color-2');
 const divColor3 = document.querySelector('#color-3');
-const getPixel = document.querySelectorAll('.pixel');
+const sectionPixelBoard = document.querySelector('#pixel-board');
+const inputPixelBoard = document.querySelector('#board-size');
 
 divColor0.className = 'color selected';
 divColor0.style.backgroundColor = 'black';
@@ -10,10 +11,67 @@ divColor1.style.backgroundColor = 'red';
 divColor2.style.backgroundColor = 'blue';
 divColor3.style.backgroundColor = 'pink';
 
+function createElement(element, text, attribute, value) {
+  const newElement = document.createElement(element);
+  newElement.innerText = text;
+  newElement.setAttribute(attribute, value);
+  return newElement;
+}
+
+for (let index = 0; index < 25; index += 1) {
+  sectionPixelBoard.appendChild(createElement('div', '', 'class', 'pixel'));
+}
+
+const getPixel = document.querySelectorAll('.pixel');
+
 function changeColorSelected(event) {
   const getSelected = document.querySelector('.selected');
   getSelected.classList.remove('selected');
   event.target.classList.add('selected');
+}
+
+// function inputMin() {
+//   const inputValue = inputPixelBoard.value;
+//   if (inputValue < 5) {
+//     for (let index = 0; index < 25; index += 1) {
+//       sectionPixelBoard.appendChild(createElement('div', '', 'class', 'pixel'));
+//     }
+//   }
+// }
+
+// function inputMax() {
+//   const inputValue = inputPixelBoard.value;
+//   if (inputValue > 50) {
+//     for (let index = 0; index < 25; index += 1) {
+//       sectionPixelBoard.appendChild(createElement('div', '', 'class', 'pixel'));
+//     }
+//   }
+// }
+
+function verifyInput() {
+  const inputValue = inputPixelBoard.value;
+  if (inputValue === '') {
+    alert('Board inválido!');
+  }
+}
+
+function changePixelBoard() {
+  sectionPixelBoard.innerHTML = '';
+  verifyInput();
+  const inputValue = inputPixelBoard.value;
+
+  const pixelSize = 40;
+  sectionPixelBoard.style.width = `${pixelSize * inputValue}px`;
+  sectionPixelBoard.style.height = `${pixelSize * inputValue}px`;
+  for (let row = 0; row < inputValue; row += 1) {
+    for (let col = 0; col < inputValue; col += 1) {
+      const pixel = sectionPixelBoard.appendChild(createElement('div', '', 'class', 'pixel'));
+      pixel.style.backgroundColor = 'white';
+      pixel.style.width = `${pixelSize}px`;
+      pixel.style.height = `${pixelSize}px`;
+      sectionPixelBoard.appendChild(pixel);
+    }
+  }
 }
 
 function createColor() {
@@ -23,20 +81,14 @@ function createColor() {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
-function colorPixel(event) {
+function colorPixel({ target }) {
   const getSelected = document.querySelector('.selected');
-  const replacementColor = getSelected.style.backgroundColor;
 
   // eslint-disable-next-line no-param-reassign
-  event.target.style.backgroundColor = replacementColor;
+  target.style.backgroundColor = getSelected.style.backgroundColor;
 
-  // Salvar o estado atual do quadro de pixels
   const currentPixelBoard = Array.from(getPixel).map((pixel) => pixel.style.backgroundColor);
   localStorage.setItem('pixelBoard', JSON.stringify(currentPixelBoard));
-}
-
-for (let index = 0; index < getPixel.length; index += 1) {
-  getPixel[index].addEventListener('click', colorPixel);
 }
 
 function savedStorage(item) {
@@ -65,8 +117,13 @@ function clearBoard() {
   }
 }
 
+for (let index = 0; index < getPixel.length; index += 1) {
+  getPixel[index].addEventListener('click', colorPixel);
+}
+
 document.querySelector('#button-random-color').addEventListener('click', randomColor);
 document.querySelector('#clear-board').addEventListener('click', clearBoard);
+document.querySelector('#generate-board').addEventListener('click', changePixelBoard);
 divColor0.addEventListener('click', changeColorSelected);
 divColor1.addEventListener('click', changeColorSelected);
 divColor2.addEventListener('click', changeColorSelected);
@@ -81,7 +138,6 @@ window.onload = () => {
     }
   }
 
-  // Recuperar o estado do quadro de pixels do localStorage
   const savedPixelBoard = JSON.parse(localStorage.getItem('pixelBoard'));
   if (savedPixelBoard) {
     const pixelArray = Array.from(getPixel);
